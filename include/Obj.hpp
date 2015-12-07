2646e775c61e23aef10ecc7673f6b968fd839c8c
DimitrisDimou <mijuomij@gmail.com>
2015-12-07 13:07:33 +0200

    Added default copy/move ctor

--- include/Obj.hpp
+++ include/Obj.hpp
@@ -17,10 +17,13 @@ using std::endl;
 
 class Vertex {
 public:
-	Vertex ();
-	Vertex (float, float, float);
-	Vertex (const Vertex&);
-	virtual ~Vertex ();
+	Vertex();
+	Vertex(float, float, float);
+	Vertex(Vertex&&);
+	Vertex(const Vertex&);
+	Vertex& operator=(Vertex&&);
+	Vertex& operator=(const Vertex&);
+	virtual ~Vertex();
 
   	float x;
 	float y;
@@ -30,10 +33,13 @@ public:
 class Triangle
 {
 public:
-	Triangle ();
-	Triangle (Vertex, Vertex, Vertex);
-	Triangle (const Triangle&);
-	virtual ~Triangle ();
+	Triangle();
+	Triangle(Vertex, Vertex, Vertex);
+	Triangle(Triangle&&);
+	Triangle(const Triangle&);
+	Triangle& operator=(Triangle&&);
+	Triangle& operator=(const Triangle&);
+	virtual ~Triangle();
 
 	Vertex v1;
 	Vertex v2;
@@ -43,26 +49,32 @@ public:
 class Pair
 {
 public:
-	Pair (int, int);
-	~Pair ();
+	Pair(int, int);
+	~Pair();
 
 	int v;
 	int n;
 };
 
 
-Vertex::Vertex (float x, float y, float z) : x(x), y(y), z(z) {}
-Vertex::Vertex () : x(0), y(0), z(0) {}
-Vertex::Vertex (const Vertex& v) : x(v.x), y(v.y), z(v.z) {}
-Vertex::~Vertex () {}
+Vertex::Vertex(float x, float y, float z) : x(x), y(y), z(z) {}
+Vertex::Vertex() : x(0), y(0), z(0) {}
+Vertex::Vertex(Vertex&& v) = default;
+Vertex::Vertex(const Vertex& v) = default;
+Vertex& Vertex::operator=(Vertex&& v) = default;
+Vertex& Vertex::operator=(const Vertex& v) = default;
+Vertex::~Vertex() {}
 
-Triangle::Triangle () : v1(Vertex(0, 0, 0)), v2(Vertex(0, 0, 0)), v3(Vertex(0, 0, 0)) {}
-Triangle::Triangle (Vertex v1, Vertex v2, Vertex v3) : v1(v1), v2(v2), v3(v3) {}
-Triangle::Triangle (const Triangle& t) : v1( t.v1 ), v2( t.v2 ), v3( t.v3 ) {}
-Triangle::~Triangle () {}
+Triangle::Triangle() : v1(Vertex(0, 0, 0)), v2(Vertex(0, 0, 0)), v3(Vertex(0, 0, 0)) {}
+Triangle::Triangle(Vertex v1, Vertex v2, Vertex v3) : v1(v1), v2(v2), v3(v3) {}
+Triangle::Triangle(Triangle&& t) = default;
+Triangle::Triangle(const Triangle& t) = default;
+Triangle& Triangle::operator=(Triangle&& t) = default;
+Triangle& Triangle::operator=(const Triangle& t) = default;
+Triangle::~Triangle() {}
 
-Pair::Pair (int x, int y) : v(x), n(y) {}
-Pair::~Pair () {}
+Pair::Pair(int x, int y) : v(x), n(y) {}
+Pair::~Pair() {}
 
 std::ifstream objecttext;
 std::string gline;
